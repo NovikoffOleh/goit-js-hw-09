cf01bc65dbd979121af72ab1e77ca2d779656f9c
NovikoffOleh <novikoff10000@gmail.com>
2022-12-04 13:06:16 +0200

    correct

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,15 +1,24 @@
 const refs = {
     startBtn: document.querySelector('button[data-start]'),
     stopBtn: document.querySelector('button[data-stop]'),
+    
   };
-  
+    const button = document.querySelector('button');
+    button.onclick = function() {
+    const name = prompt('Введіть ваше ім"я');
+    alert('Вітаю ' + name + ', якщо є світло, можеш розважатися');
+    }
+
   function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    
   }
   
   function updateBodyBGcolor(color) {
     document.body.style.backgroundColor = color;
+    
   }
+  
   
   class ColorSwitcher {
     constructor(updateBodyBGcolor) {
